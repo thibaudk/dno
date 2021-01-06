2d6e8f71589a75037ad4c67a4c023ad3f822092b
thibaudk <tibthejazzdude@gmail.com>
2021-01-06 02:48:45 +0100

    basic version

--- src/DeNoiser.hpp
+++ src/DeNoiser.hpp
@@ -1,2 +1,22 @@
 #pragma once
-#include <dno/include/1efilter.hpp>
+#include <libossia/3rdparty/dno/include/1efilter.hpp>
+
+template <typename T = double>
+class DeNoiser
+{
+  public :
+    DeNoiser(const double frequency = 120,
+                const T minCutoff = 1,
+                const T beta = 1,
+                const T dCutoff = 1)
+      : one_euro{one_euro_filter<T>(frequency, minCutoff, beta, dCutoff)}
+    {}
+
+    T operator()(T val)
+    {
+      return one_euro(val, 1);
+    }
+
+  protected :
+    one_euro_filter<T> one_euro;
+};
