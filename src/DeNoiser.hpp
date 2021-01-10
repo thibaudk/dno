4c36a7370a26b9d562fe014c46ce34be7aaeb8d1
thibaudk <tibthejazzdude@gmail.com>
2021-01-10 10:07:38 +0100

    update

--- src/DeNoiser.hpp
+++ src/DeNoiser.hpp
@@ -1,20 +1,63 @@
 #pragma once
-#include <libossia/3rdparty/dno/include/1efilter.hpp>
+#include "../include/1efilter.hpp"
+
+/*
+ * For more information about "the one euro filter"
+ * please visit https://cristal.univ-lille.fr/~casiez/1euro/
+ *
+ * * quote * "Note that parameters fcmin and beta have clear
+ * conceptual relationships: if high speed lag is a problem,
+ * increase beta; if slow speed jitter is a problem, decrease
+ * fcmin."
+ *
+ */
+
+#define INIT_FREQ 120
+#define INIT_AMOUNT 1
+#define INIT_BETA 1
+#define INIT_DCUTOFF 1
 
 template <typename T = double>
 class DeNoiser
 {
   public :
-    DeNoiser(const double frequency = 120,
-                const T minCutoff = 1,
-                const T beta = 1,
-                const T dCutoff = 1)
-      : one_euro{one_euro_filter<T>(frequency, minCutoff, beta, dCutoff)}
+    DeNoiser()
+      : one_euro{INIT_FREQ,
+                 T{INIT_AMOUNT},
+                 T{INIT_BETA},
+                 T{INIT_DCUTOFF}}
     {}
 
     T operator()(T val)
     {
-      return one_euro(val, 1);
+      return one_euro(val);
+    }
+
+    void setAmount(float amount = INIT_AMOUNT)
+    {
+      amount *= 10;
+
+      if (amount <= 0)
+      {
+        amount = 0.0001;
+      }
+
+      one_euro.mincutoff = static_cast<T>(10 - amount);
+    }
+
+    void set1eFreq(double freq = INIT_FREQ)
+    {
+      one_euro.frequency = freq;
+    }
+
+    void set1eBeta(T beta = INIT_BETA)
+    {
+      one_euro.beta = beta;
+    }
+
+    void set1eDcutOff(T cutoff = INIT_DCUTOFF)
+    {
+      one_euro.dcutoff = cutoff;
     }
 
   protected :
